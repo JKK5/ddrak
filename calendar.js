fe0400c6afd4af6a1a968859310d8db33030f10c
JKK5 <jshane@g.skku.edu>
2021-01-23 01:29:43 +0900

    0123-1

--- calendar.js
+++ calendar.js
@@ -18,14 +18,46 @@ const months = [
   "December",
 ];
 const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-console.log(year, month + 1, date, daysOfWeek[day]);
 
 function getDateGrid(year, month) {
   let dates = [];
   const firstDay = new Date(year, month).getDay();
   const totalDaysThisMonth = new Date(year, month + 1, 0).getDate();
   const totalDaysPrevMonth = new Date(year, month, 0).getDate();
+
+  for (let i = 1; i <= firstDay; i++) {
+    let prevMonthDate = totalDaysPrevMonth - firstDay + i;
+    let key = new Date(year, month - 1, prevMonthDate);
+    dates.push({ key: key, date: prevMonthDate, monthClass: "prev" });
+  }
+  for (let i = 1; i <= totalDaysThisMonth; i++) {
+    let key = new Date(year, month, i);
+    dates.push({ key: key, date: i, monthClass: "current" });
+  }
+  let gridsLeft = 42 - dates.length;
+  for (let i = 1; i <= gridsLeft; i++) {
+    let key = new Date(year, month, i);
+    dates.push({ key: key, date: i, monthClass: "next" });
+  }
+  return dates;
 }
 
-const header = document.querySelector(".header");
-header.innerHTML = `${months[month]} ${year}`;
+function render() {
+  const calendar = document.querySelector(".calendar");
+  calendar.innerHTML = `
+    <div class="calendar-nav">
+      <button>Prev</button>
+      <h2>${months[month]} ${year}</h2>
+      <button>Next</button>
+    </div>
+    <div class="calendar-day-grid">
+      ${daysOfWeek.map((day) => `<div>${day}</div>`).join("")}
+    </div>
+    <div class="calendar-date-grid">
+      ${getDateGrid(year, month)
+        .map((date) => `<div class="${date.monthClass}">${date.date}</div>`)
+        .join("")}
+    </div>
+  `;
+}
+render();
