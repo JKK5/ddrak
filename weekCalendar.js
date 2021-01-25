fe6d721956a265d2362448a09a3c9e1612c19103
JKK5 <jshane@g.skku.edu>
2021-01-25 21:51:38 +0900

    Make nav buttons work

--- weekCalendar.js
+++ weekCalendar.js
@@ -14,13 +14,7 @@ const months = [
 ];
 const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-const state = {
-  year: new Date().getFullYear(),
-  month: new Date().getMonth(),
-  date: new Date().getDate(),
-};
-
-const curr = new Date();
+let curr = new Date();
 
 function getDateGrid() {
   const week = [];
@@ -35,13 +29,13 @@ function getDateGrid() {
       date: key.getDate(),
       day: i,
     });
-    console.log(week);
   }
   return week;
 }
 
 function getTitle() {
   const week = getDateGrid();
+  let title = "";
   const first = {
     y: week[0].year,
     m: week[0].month,
@@ -52,31 +46,36 @@ function getTitle() {
     m: week[6].month,
     d: week[6].date,
   };
+
   if (first.y !== last.y) {
-    return `${months[first.m]} ${first.d}, ${first.y} - ${months[last.m]} ${
+    title = `${months[first.m]} ${first.d}, ${first.y} - ${months[last.m]} ${
       last.d
     }, ${last.y}`;
   } else {
     if (first.m !== last.m) {
-      return `${months[first.m]} ${first.d} - ${months[last.m]} ${last.d}, ${
+      title = `${months[first.m]} ${first.d} - ${months[last.m]} ${last.d}, ${
         last.y
       }`;
     } else {
-      return `${months[first.m]} ${first.d} - ${last.d}, ${last.y}`;
+      title = `${months[first.m]} ${first.d} - ${last.d}, ${last.y}`;
     }
   }
+  return title;
 }
+
+let weekCnt = 0;
 
 function render() {
   const calendar = document.querySelector(".calendar");
   calendar.innerHTML = `
     <div class="calendar-nav">
       <h2>${getTitle()}</h2>
-      <button id="prevMonth">Prev</button>
-      <button id="nextMonth">Next</button>
-      <button id="todayMonth">Today</button>
+      <button id="prevWeek">Prev</button>
+      <button id="nextWeek">Next</button>
+      <button id="todayWeek">Today</button>
     </div>
     <div class="calendar-date-grid">
+      <div></div>
       ${getDateGrid()
         .map(
           (week) =>
@@ -87,4 +86,25 @@ function render() {
   `;
 }
 
-render();
+function showCalendar(prevNextInd) {
+  curr.setDate(curr.getDate() + prevNextInd);
+  weekCnt -= prevNextInd;
+  render();
+}
+
+function init() {
+  showCalendar(0);
+  console.log;
+
+  document.addEventListener("click", function (event) {
+    if (event.target.id === "prevWeek") {
+      showCalendar(-7);
+    } else if (event.target.id === "nextWeek") {
+      showCalendar(7);
+    } else if (event.target.id === "todayWeek") {
+      showCalendar(weekCnt);
+    }
+  });
+}
+
+init();
